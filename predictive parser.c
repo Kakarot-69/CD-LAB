234936ccb3cfad6783a86257e0e971b015aa05d8
Kakarot-69 <69302378+Kakarot-69@users.noreply.github.com>
2021-01-07 10:51:17 +0530

    removed the error

--- predictive parser.c
+++ predictive parser.c
@@ -1,74 +1,151 @@
 #include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
+
+#include<conio.h>
+
 #include<string.h>
-void err(const char* s)
+
+char prol[7][10]={"S","A","A","B","B","C","C"};
+
+char pror[7][10]={"A","Bb","Cd","aB","@","Cc","@"};
+
+char prod[7][10]={"S->A","A->Bb","A->Cd","B->aB","B->@","C->Cc","C->@"}; 
+
+char first[7][10]={"abcd","ab","cd","a@","@","c@","@"};
+
+char follow[7][10]={"$","$","$","a$","b$","c$","d$"};
+
+char table[5][6][10];
+
+numr(char c)
+
 {
-perror(s);
-exit(0);
-}
-int factor()
+
+switch(c)
+
 {
-int val,i;
-char ch[0];
-scanf("%s",ch);
-switch(ch[0])
+
+case 'S': return 0;
+
+case 'A': return 1;
+
+case 'B': return 2;
+
+case 'C': return 3;
+
+case 'a': return 0;
+
+case 'b': return 1;
+
+case 'c': return 2;
+
+case 'd': return 3;
+
+case '$': return 4;
+
+}
+
+return(2);
+
+}
+
+void main()
+
 {
-case '(':
-val=expr();
-scanf("%s",ch);
-if(ch[0]!=')')
-err("Missing closing paranthesis in factor.");
-break;
-default :{
-for(i=0;i<strlen(ch);i++)
+
+int i,j,k;
+
+clrscr();
+
+for(i=0;i<5;i++)
+
+for(j=0;j<6;j++)
+
+strcpy(table[i][j]," ");
+
+printf("\nThe following is the predictive parsing table for the following grammar:\n");
+
+for(i=0;i<7;i++)
+
+printf("%s\n",prod[i]);
+
+printf("\nPredictive parsing table is\n");
+
+fflush(stdin);
+
+for(i=0;i<7;i++)
+
 {
-if((ch[i]>'0')&&(ch[i]<='9'))
-continue;
-else
-err("Illegal character sequence in place of factor.");
+
+k=strlen(first[i]);
+
+for(j=0;j<10;j++)
+
+if(first[i][j]!='@')
+
+strcpy(table[numr(prol[i][0])+1][numr(first[i][j])+1],prod[i]);
+
 }
-val=atoi(ch);
-}
-}
-return val;
-}
-int term()
+
+for(i=0;i<7;i++)
+
 {
-int val;
-char ch[10];
-val=factor();
-while(1)
+
+if(strlen(pror[i])==1)
+
 {
-scanf("%s",ch);
-if(ch[0]=='*')
+
+if(pror[i][0]=='@')
+
 {
-val=val*factor();
+
+k=strlen(follow[i]);
+
+for(j=0;j<k;j++)
+
+strcpy(table[numr(prol[i][0])+1][numr(follow[i][j])+1],prod[i]);
+
 }
-else
-break;
+
 }
-ungetc(ch[0],stdin);
-return val;
+
 }
-int expr()
+
+strcpy(table[0][0]," ");
+
+strcpy(table[0][1],"a");
+
+strcpy(table[0][2],"b");
+
+strcpy(table[0][3],"c");
+
+strcpy(table[0][4],"d");
+
+strcpy(table[0][5],"$");
+
+strcpy(table[1][0],"S");
+
+strcpy(table[2][0],"A");
+
+strcpy(table[3][0],"B");
+
+strcpy(table[4][0],"C");
+
+printf("\n--------------------------------------------------------\n");
+
+for(i=0;i<5;i++)
+
+for(j=0;j<6;j++)
+
 {
-int val;
-char ch[10];
-val=term();
-while(1)
-{
-scanf("%s",ch);
-if(ch[0]=='+')
-val=val+term();
-else
-break;
+
+printf("%-10s",table[i][j]);
+
+if(j==5)
+
+printf("\n--------------------------------------------------------\n");
+
 }
-ungetc(ch[0],stdin);
-return val;
-}
-main()
-{
-printf("\nEnter the expression: ");
-printf("\n Result: %d\n",expr());
+
+getch();
+
 }
